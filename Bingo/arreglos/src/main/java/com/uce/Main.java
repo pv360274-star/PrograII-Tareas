75b6e38f5524d5b311e95470d6859562bc5b03e6
Pedro Vega <pv360274@gmail.com>
2026-05-28 01:27:24 -0500

    Actualizando Bingo

--- Bingo/arreglos/src/main/java/com/uce/Main.java
+++ Bingo/arreglos/src/main/java/com/uce/Main.java
@@ -1,70 +1,56 @@
 package com.uce;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String[][] bingo = new String[5][5];
-        Random rnd = new Random();
+    public static void main(String[] args) {String [][] bingo = new String [5][5];
         Scanner sc = new Scanner(System.in);
 
-        // 1. Llenar matriz con números aleatorios (1 al 99)
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                bingo[i][j] = String.valueOf(rnd.nextInt(99) + 1);
+        boolean[] usados = new boolean[100]; 
+        
+        for(int i=0; i<bingo.length; i++){
+            for(int j=0; j<bingo[i].length; j++){
+                int numeroAleatorio;
+
+                do {
+                    numeroAleatorio = (int)(Math.random() * 100);
+                } while (usados[numeroAleatorio] == true);
+                
+                usados[numeroAleatorio] = true;
+                bingo[i][j] = String.valueOf(numeroAleatorio);
             }
         }
 
-        System.out.println(" Tabla de Bingo ");
-        System.out.println("---------------------------------");
-        for (int turno = 1; turno <= 25; turno++) {
+        System.out.println("---------- Tabla de Bingo ----------");
+        for(int i=0; i<bingo.length; i++){
+            for(int j=0; j<bingo[i].length; j++){
+                System.out.print(bingo[i][j] + "\t");
+            }
+            System.out.println();
+        }
 
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-    
+        while(true){
+            System.out.println("Ingrese una fila:");
+            int fila = sc.nextInt();
+            if(fila == -1){
+                break;
+            }
+            
+            System.out.println("Ingrese una columna:");
+            int columna = sc.nextInt();
+
+            if (fila >= 0 && fila < 5 && columna >= 0 && columna < 5) {
+                bingo[fila][columna] = "x";
+            } else {
+                System.out.println("Ese índice no existe. Solo usa números del 0 al 4.");
+                continue; 
+            }
+            
+            System.out.println("---------- Tabla de Bingo ----------");
+            for(int i=0; i<bingo.length; i++){
+                for(int j=0; j<bingo[i].length; j++){
                     System.out.print(bingo[i][j] + "\t");
                 }
                 System.out.println();
-            }
-            System.out.println("---------------------------------");
-
-            System.out.print("\nIngrese su número: ");
-            String num = sc.nextLine();
-
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    if (bingo[i][j].equals(num)) {
-                        bingo[i][j] = "X";
-                    }
-                }
-            }
-
-            boolean gano = false;
-            for (int i = 0; i < 5; i++) {
-                int contadorFila = 0;
-                int contadorCol = 0;
-                
-                for (int j = 0; j < 5; j++) {
-                    if (bingo[i][j].equals("X")) contadorFila++;
-                    if (bingo[j][i].equals("X")) contadorCol++;
-                }
-                
-                if (contadorFila == 5 || contadorCol == 5) {
-                    gano = true;
-                }
-            }
-
-            if (gano) {
-                System.out.println("B\tI\tN\tG\tO");
-                System.out.println("---------------------------------");
-                for (int i = 0; i < 5; i++) {
-                    for (int j = 0; j < 5; j++) {
-                        System.out.print(  bingo[i][j] + "\t");
-                    }
-                    System.out.println();
-                }
-                System.out.println("\n Bingoooooooo");
-                break; 
             }
         }
         sc.close();
