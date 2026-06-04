91cc24d6c9a6e59b95fa53ce2d042e50efc74fcc
Pedro <pv360274@gmail.com>
2026-06-04 17:46:47 -0500

    Actualizacion Bingo

--- Bingo/arreglos/src/main/java/com/uce/Main.java
+++ Bingo/arreglos/src/main/java/com/uce/Main.java
@@ -1,38 +1,45 @@
 package com.uce;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] bingo = new String[5][5];
+
+        String[][] bingo = new String[5][];
+
         Random rnd = new Random();
         Scanner sc = new Scanner(System.in);
 
-        // 1. Llenar matriz con números aleatorios (1 al 99)
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < bingo.length; i++) {
+            bingo[i] = new String[rnd.nextInt(3) + 3];
+        }
+
+        for (int i = 0; i < bingo.length; i++) {
+            for (int j = 0; j < bingo[i].length; j++) {
                 bingo[i][j] = String.valueOf(rnd.nextInt(99) + 1);
             }
         }
 
         System.out.println(" Tabla de Bingo ");
         System.out.println("---------------------------------");
-        for (int turno = 1; turno <= 25; turno++) {
 
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-    
+        for (int turno = 1; turno <= 100; turno++) {
+
+            for (int i = 0; i < bingo.length; i++) {
+                for (int j = 0; j < bingo[i].length; j++) {
                     System.out.print(bingo[i][j] + "\t");
                 }
                 System.out.println();
             }
+
             System.out.println("---------------------------------");
 
             System.out.print("\nIngrese su número: ");
             String num = sc.nextLine();
 
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < bingo.length; i++) {
+                for (int j = 0; j < bingo[i].length; j++) {
                     if (bingo[i][j].equals(num)) {
                         bingo[i][j] = "X";
                     }
@@ -40,33 +47,38 @@ public class Main {
             }
 
             boolean gano = false;
-            for (int i = 0; i < 5; i++) {
+
+            for (int i = 0; i < bingo.length; i++) {
                 int contadorFila = 0;
-                int contadorCol = 0;
-                
-                for (int j = 0; j < 5; j++) {
-                    if (bingo[i][j].equals("X")) contadorFila++;
-                    if (bingo[j][i].equals("X")) contadorCol++;
+
+                for (int j = 0; j < bingo[i].length; j++) {
+                    if (bingo[i][j].equals("X")) {
+                        contadorFila++;
+                    }
                 }
-                
-                if (contadorFila == 5 || contadorCol == 5) {
+
+                if (contadorFila == bingo[i].length) {
                     gano = true;
+                    break;
                 }
             }
 
             if (gano) {
                 System.out.println("B\tI\tN\tG\tO");
                 System.out.println("---------------------------------");
-                for (int i = 0; i < 5; i++) {
-                    for (int j = 0; j < 5; j++) {
-                        System.out.print(  bingo[i][j] + "\t");
+
+                for (int i = 0; i < bingo.length; i++) {
+                    for (int j = 0; j < bingo[i].length; j++) {
+                        System.out.print(bingo[i][j] + "\t");
                     }
                     System.out.println();
                 }
-                System.out.println("\n Bingoooooooo");
-                break; 
+
+                System.out.println("\nBingoooooooo");
+                break;
             }
         }
+
         sc.close();
     }
 }
